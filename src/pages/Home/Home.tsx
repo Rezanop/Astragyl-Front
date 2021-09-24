14ce10467ee4965bc35daca6a089e4ae1089a70a
RezanOp <nacer.k2@gmail.com>
2021-09-24 23:28:56 +0200

    Fix Home Component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,11 @@
 import React, { FC } from 'react';
 
-
 const Home: FC = () => {
-
-    return (
-        <>
-            <p>Home Page</p>
-        </>
-    );
+  return (
+    <>
+      <p>Home Page</p>
+    </>
+  );
 };
 
 export default Home;
-
-
-
