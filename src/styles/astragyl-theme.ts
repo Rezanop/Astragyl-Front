52b5066e2b41788f9b28ebcbd63c4552cd8a4104
RezanOp <nacer.k2@gmail.com>
2021-09-26 13:16:58 +0200

    Add Styled-Component and Theming setup

---
+++ src/styles/astragyl-theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+const astragylTheme: DefaultTheme = {
+  colors: {
+    primary: 'red',
+    secondary: 'green',
+  },
+};
+
+export { astragylTheme };
